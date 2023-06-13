5e2ab3c1cbb94e82009488bbc4cf97ee498d1e2c
deepak <deepakvyas261092@gmail.com>
2023-06-13 18:43:51 +0530

    Video

--- resources/views/web/pages/gallery/video.blade.php
+++ resources/views/web/pages/gallery/video.blade.php
@@ -12,7 +12,7 @@
 <section class="py-3">
     <div class="container">
         <div class="row">
-            @for ($i = 1; $i <= 8; $i++) <div class="col-sm-3 text-center mb-3" data-toggle="modal" data-target="#exampleModal">
+            @for ($i = 1; $i <= 8; $i++) <div class="col-sm-3 text-center mb-3">
                 <div class="position-relative">
                     <img src="https://sysinventory.in/catalogue/assets/img/banner/m02-banner6.jpg" class="w-100 object-fit-cover" style="height: 250px;" data-toggle="modal" data-target="#exampleModalLong">
 
@@ -22,9 +22,39 @@
                 </div>
         </div>
         @endfor
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            Launch demo modal
+        </button>
 
+        <!-- Modal -->
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--End modal-->
     </div>
     </div>
+
 </section>
 <!--video gallery end-->
 @endsection
